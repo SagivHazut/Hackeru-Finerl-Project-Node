57db808a7a38fd8550288fa361f8b6d5afca60e6
sagiv hazut <sagivhazut@gmail.com>
2022-05-05 18:04:31 +0300

    first commit

---
+++ Routes/Womencard/cardValidation.js
@@ -0,0 +1,12 @@
+const Joi = require("joi");
+
+function validateCard(womenCard) {
+  const schema = Joi.object({
+    name: Joi.string().min(2).max(256).required(),
+    description: Joi.string().min(10).max(1024).required(),
+    phone: Joi.string().min(2).max(14).required(),
+    image: Joi.string().min(11).max(256),
+  });
+  return schema.validate(womenCard);
+}
+exports.validateCard = validateCard;
